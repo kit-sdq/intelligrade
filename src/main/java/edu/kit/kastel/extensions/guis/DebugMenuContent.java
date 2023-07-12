472e98037214ce91d86a65cbbe2d1a27062a2b04
CDaut <git@cdaut.de>
2023-07-13 01:23:30 +0200

    added field for Artemis URL

--- src/main/java/edu/kit/kastel/extensions/guis/DebugMenuContent.java
+++ src/main/java/edu/kit/kastel/extensions/guis/DebugMenuContent.java
@@ -40,6 +40,8 @@ public class DebugMenuContent extends JPanel {
     label3 = new JLabel();
     loggedInLabel = new JLabel();
     separator1 = new JSeparator();
+    label4 = new JLabel();
+    textField1 = new JTextField();
     label1 = new JLabel();
     InputUsername = new JTextField();
     label2 = new JLabel();
@@ -47,19 +49,20 @@ public class DebugMenuContent extends JPanel {
     btnLogin = new JButton();
 
     //======== this ========
-    setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
-    . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing
-    .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
-    Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
-    ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
-    public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062order" .equals ( e. getPropertyName (
-    ) ) )throw new RuntimeException( ) ;} } );
+    setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax
+    . swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing
+    . border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .
+    Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
+    ) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override
+    public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName (
+    ) )) throw new RuntimeException( ); }} );
     setLayout(new MigLayout(
       "fillx,insets 0,hidemode 3,align left top,gap 0 0",
       // columns
-      "[90,fill]" +
+      "[109,fill]" +
       "[grow,fill]",
       // rows
+      "[]" +
       "[]" +
       "[]" +
       "[top]" +
@@ -75,19 +78,27 @@ public class DebugMenuContent extends JPanel {
     add(loggedInLabel, "cell 1 0");
     add(separator1, "cell 0 1 2 1");
 
+    //---- label4 ----
+    label4.setText(bundle.getString("DebugMenuContent.label4.text"));
+    add(label4, "cell 0 2");
+
+    //---- textField1 ----
+    textField1.setText(bundle.getString("DebugMenuContent.textField1.text"));
+    add(textField1, "cell 1 2");
+
     //---- label1 ----
     label1.setText(bundle.getString("labelUnameField"));
-    add(label1, "cell 0 2,alignx left,growx 0");
-    add(InputUsername, "cell 1 2,growx");
+    add(label1, "cell 0 3,alignx left,growx 0");
+    add(InputUsername, "cell 1 3,growx");
 
     //---- label2 ----
     label2.setText(bundle.getString("LabelPwdField"));
-    add(label2, "cell 0 3,alignx left,growx 0");
-    add(inputPwd, "cell 1 3,growx");
+    add(label2, "cell 0 4,alignx left,growx 0");
+    add(inputPwd, "cell 1 4,growx");
 
     //---- btnLogin ----
     btnLogin.setText("log in");
-    add(btnLogin, "cell 0 4 2 1");
+    add(btnLogin, "cell 0 5 2 1");
     // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
   }
 
@@ -96,6 +107,8 @@ public class DebugMenuContent extends JPanel {
   private JLabel label3;
   private JLabel loggedInLabel;
   private JSeparator separator1;
+  private JLabel label4;
+  private JTextField textField1;
   private JLabel label1;
   private JTextField InputUsername;
   private JLabel label2;
