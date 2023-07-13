e8e81e4255004c755cebb3252ed70762991bed56
CDaut <git@cdaut.de>
2023-07-13 11:20:46 +0200

    added manual login functionality

--- src/main/java/edu/kit/kastel/extensions/MainMenuFactory.java
+++ src/main/java/edu/kit/kastel/extensions/MainMenuFactory.java
@@ -3,19 +3,26 @@ package edu.kit.kastel.extensions;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import edu.kit.kastel.extensions.guis.DebugMenuContent;
+import edu.kit.kastel.sdq.artemis4j.api.ArtemisClientException;
+import edu.kit.kastel.sdq.artemis4j.client.RestClientManager;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 
 public class MainMenuFactory implements ToolWindowFactory {
   private static final String WINDOW_TITLE = "Debug view";
+  private static final String LOGIN_ERROR_DIALOG_TITLE = "Error logging in!";
 
   //set up automated GUI and generate necessary bindings
   private final JPanel contentPanel = new JPanel();
@@ -23,23 +30,51 @@ public class MainMenuFactory implements ToolWindowFactory {
   private final JPasswordField pwdInput = generatedMenu.getInputPwd();
   private final JTextField usernameField = generatedMenu.getInputUsername();
   private final JLabel loggedInLabel = generatedMenu.getLoggedInLabel();
+  private final JTextField artemisUrlField = generatedMenu.getArtemisUrlInput();
 
   @Override
   public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
     contentPanel.setLayout(new GridLayout());
     //add action listener to login Button
-    //TODO: move this to a separate method
-    generatedMenu.getBtnLogin().addActionListener(actionEvent ->
-            System.out.printf("Username: %s Password: %s%n", usernameField.getText(), String.valueOf(pwdInput.getPassword()))
-    );
+    generatedMenu.getBtnLogin().addActionListener(this::loginButtonListener);
+
     //add content to menu panel
     contentPanel.add(generatedMenu);
-    Content content = ContentFactory.SERVICE.getInstance().createContent(
+    Content content = ContentFactory.getInstance().createContent(
             this.contentPanel,
             WINDOW_TITLE,
             false
     );
     toolWindow.getContentManager().addContent(content);
+  }
+
+  /**
+   * Listener Method that gets called when the login Button is pressed
+   * This method will Log in the User
+   *
+   * @param actionEvent The Event passed by AWT is the Button is pressed
+   */
+  private void loginButtonListener(ActionEvent actionEvent) {
+    //create new Artemis Instance
+    var artemisInstance = new RestClientManager(
+            artemisUrlField.getText(),
+            usernameField.getText(),
+            new String(pwdInput.getPassword())
+    );
+
+    //try logging in and display error iff error ocurred
+    try {
+      artemisInstance.login();
+    } catch (ArtemisClientException e) {
+      JOptionPane.showMessageDialog(contentPanel, e.getMessage(), LOGIN_ERROR_DIALOG_TITLE, JOptionPane.ERROR_MESSAGE);
+    }
+
+    //set label if login was successful
+    if (artemisInstance.isReady()) {
+      loggedInLabel.setText("true");
+      loggedInLabel.setForeground(JBColor.GREEN);
+    }
+
   }
 
 }
--- src/main/java/edu/kit/kastel/extensions/guis/DebugMenuContent.java
+++ src/main/java/edu/kit/kastel/extensions/guis/DebugMenuContent.java
@@ -4,6 +4,7 @@
 
 package edu.kit.kastel.extensions.guis;
 
+import java.awt.*;
 import java.util.*;
 import javax.swing.*;
 import net.miginfocom.swing.*;
@@ -32,6 +33,10 @@ public class DebugMenuContent extends JPanel {
     return loggedInLabel;
   }
 
+  public JTextField getArtemisUrlInput() {
+    return artemisUrlInput;
+  }
+
 
   private void initComponents() {
     // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
@@ -41,7 +46,7 @@ public class DebugMenuContent extends JPanel {
     loggedInLabel = new JLabel();
     separator1 = new JSeparator();
     label4 = new JLabel();
-    textField1 = new JTextField();
+    artemisUrlInput = new JTextField();
     label1 = new JLabel();
     InputUsername = new JTextField();
     label2 = new JLabel();
@@ -49,13 +54,13 @@ public class DebugMenuContent extends JPanel {
     btnLogin = new JButton();
 
     //======== this ========
-    setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax
-    . swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing
-    . border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .
-    Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
-    ) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override
-    public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName (
-    ) )) throw new RuntimeException( ); }} );
+    setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
+    . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing
+    .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
+    Font ( "Dialo\u0067", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
+    ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
+    public void propertyChange (java . beans. PropertyChangeEvent e) { if( "borde\u0072" .equals ( e. getPropertyName (
+    ) ) )throw new RuntimeException( ) ;} } );
     setLayout(new MigLayout(
       "fillx,insets 0,hidemode 3,align left top,gap 0 0",
       // columns
@@ -75,6 +80,7 @@ public class DebugMenuContent extends JPanel {
 
     //---- loggedInLabel ----
     loggedInLabel.setText(bundle.getString("DebugMenuContent.loggedInLabel.text"));
+    loggedInLabel.setForeground(Color.red);
     add(loggedInLabel, "cell 1 0");
     add(separator1, "cell 0 1 2 1");
 
@@ -82,9 +88,9 @@ public class DebugMenuContent extends JPanel {
     label4.setText(bundle.getString("DebugMenuContent.label4.text"));
     add(label4, "cell 0 2");
 
-    //---- textField1 ----
-    textField1.setText(bundle.getString("DebugMenuContent.textField1.text"));
-    add(textField1, "cell 1 2");
+    //---- artemisUrlInput ----
+    artemisUrlInput.setText(bundle.getString("DebugMenuContent.artemisUrlInput.text"));
+    add(artemisUrlInput, "cell 1 2");
 
     //---- label1 ----
     label1.setText(bundle.getString("labelUnameField"));
@@ -108,7 +114,7 @@ public class DebugMenuContent extends JPanel {
   private JLabel loggedInLabel;
   private JSeparator separator1;
   private JLabel label4;
-  private JTextField textField1;
+  private JTextField artemisUrlInput;
   private JLabel label1;
   private JTextField InputUsername;
   private JLabel label2;
