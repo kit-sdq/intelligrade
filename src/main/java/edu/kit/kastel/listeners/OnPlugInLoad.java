6f3304aa8d1e101bdf43b1ae6df6f52a34af7fac
CDaut <git@cdaut.de>
2023-07-14 14:51:35 +0200

    Error is now displayed as Balloon

--- src/main/java/edu/kit/kastel/listeners/OnPlugInLoad.java
+++ src/main/java/edu/kit/kastel/listeners/OnPlugInLoad.java
@@ -1,10 +1,19 @@
 package edu.kit.kastel.listeners;
 
 import com.intellij.ide.AppLifecycleListener;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import edu.kit.kastel.sdq.artemis4j.api.ArtemisClientException;
 import edu.kit.kastel.utils.ArtemisUtils;
 import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import java.util.List;
 
@@ -21,12 +30,17 @@ public class OnPlugInLoad implements AppLifecycleListener {
     try {
       ArtemisUtils.getNewArtemisInstance();
     } catch (ArtemisClientException clientException) {
-      JOptionPane.showMessageDialog(
-              null,
-              "Error logging in to Artemis: " + clientException.getMessage(),
-              LOGIN_ERROR_DIALOG_TITLE,
-              JOptionPane.ERROR_MESSAGE
-      );
+
+      //create Balloon notification indicating error & fix
+      NotificationGroupManager.getInstance()
+              .getNotificationGroup("IntelliGrade Notifications")
+              .createNotification(
+                      String.format("%s. This will make the grading PlugIn unusable!%n", clientException.getMessage()),
+                      NotificationType.ERROR)
+              .setTitle("IntelliGrade Login error")
+              .addAction(new OpenSettingsAction("Configure..."))
+              .notify(null);
+
     }
   }
 }
