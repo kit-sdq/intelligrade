35f40c51b16d0d8877b427a84999f5a8d001fa7a
CDaut <git@cdaut.de>
2023-07-14 02:15:48 +0200

    built more UI

--- src/main/java/edu/kit/kastel/extensions/guis/AssesmentViewContent.java
+++ src/main/java/edu/kit/kastel/extensions/guis/AssesmentViewContent.java
@@ -8,9 +8,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import com.intellij.openapi.ui.*;
 import com.intellij.ui.components.*;
 import com.intellij.ui.table.*;
+import com.jgoodies.forms.factories.*;
 import net.miginfocom.swing.*;
 
 /**
@@ -46,6 +48,24 @@ public class AssesmentViewContent extends JPanel {
     label6 = new JLabel();
     autograderConfigPathInput = new TextFieldWithBrowseButton();
     separator1 = new JSeparator();
+    generalPanel = new JPanel();
+    btnGradingRound1 = new JButton();
+    btnGradingRound2 = new JButton();
+    button5 = new JButton();
+    assessmentPanel = new JPanel();
+    button1 = new JButton();
+    button2 = new JButton();
+    button3 = new JButton();
+    button4 = new JButton();
+    panel5 = new JPanel();
+    label8 = new JLabel();
+    StatisticsContainer = new JLabel();
+    panel3 = new JPanel();
+    label7 = new JLabel();
+    comboBox4 = new ComboBox();
+    panel4 = new JPanel();
+    button6 = new JButton();
+    button7 = new JButton();
     GradingPanel = new JPanel();
     panel1 = new JPanel();
     label4 = new JLabel();
@@ -53,12 +73,13 @@ public class AssesmentViewContent extends JPanel {
     testResultsTable = new JBTable();
 
     //======== this ========
-    setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.
-    border.EmptyBorder(0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border.TitledBorder.CENTER
-    ,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font
-    .BOLD,12),java.awt.Color.red), getBorder())); addPropertyChangeListener(
-    new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062ord\u0065r"
-    .equals(e.getPropertyName()))throw new RuntimeException();}});
+    setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax
+    .swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e",javax.swing
+    .border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.
+    Font("Dialo\u0067",java.awt.Font.BOLD,12),java.awt.Color.red
+    ), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override
+    public void propertyChange(java.beans.PropertyChangeEvent e){if("borde\u0072".equals(e.getPropertyName(
+    )))throw new RuntimeException();}});
     setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
     //======== tabbedPane1 ========
@@ -77,7 +98,11 @@ public class AssesmentViewContent extends JPanel {
           "[]" +
           "[30]" +
           "[35:30]" +
-          "[10:10]"));
+          "[10:10]" +
+          "[]" +
+          "[]" +
+          "[]" +
+          "[]"));
 
         //---- label1 ----
         label1.setText(bundle.getString("AssesmentViewContent.label1.text"));
@@ -104,6 +129,113 @@ public class AssesmentViewContent extends JPanel {
         AssessmentPanel.add(label6, "cell 0 4");
         AssessmentPanel.add(autograderConfigPathInput, "cell 1 4");
         AssessmentPanel.add(separator1, "cell 0 5 2 1");
+
+        //======== generalPanel ========
+        {
+          generalPanel.setBorder(new CompoundBorder(
+            new TitledBorder(new LineBorder(Color.darkGray, 1, true), bundle.getString("AssesmentViewContent.generalPanel.border")),
+            new EmptyBorder(5, 5, 5, 5)));
+          generalPanel.setForeground(Color.blue);
+          generalPanel.setLayout(new BorderLayout());
+
+          //---- btnGradingRound1 ----
+          btnGradingRound1.setText(bundle.getString("AssesmentViewContent.btnGradingRound1.text"));
+          generalPanel.add(btnGradingRound1, BorderLayout.CENTER);
+
+          //---- btnGradingRound2 ----
+          btnGradingRound2.setText(bundle.getString("AssesmentViewContent.btnGradingRound2.text"));
+          generalPanel.add(btnGradingRound2, BorderLayout.NORTH);
+
+          //---- button5 ----
+          button5.setText(bundle.getString("AssesmentViewContent.button5.text"));
+          generalPanel.add(button5, BorderLayout.SOUTH);
+        }
+        AssessmentPanel.add(generalPanel, "cell 0 6 2 1,growx");
+
+        //======== assessmentPanel ========
+        {
+          assessmentPanel.setBorder(new CompoundBorder(
+            new TitledBorder(new LineBorder(Color.darkGray, 1, true), bundle.getString("AssesmentViewContent.assessmentPanel.border")),
+            new EmptyBorder(5, 5, 5, 5)));
+          assessmentPanel.setLayout(new GridBagLayout());
+          ((GridBagLayout)assessmentPanel.getLayout()).columnWidths = new int[] {0, 0, 0};
+          ((GridBagLayout)assessmentPanel.getLayout()).rowHeights = new int[] {0, 0, 0};
+          ((GridBagLayout)assessmentPanel.getLayout()).columnWeights = new double[] {1.0, 1.0, 1.0E-4};
+          ((GridBagLayout)assessmentPanel.getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0E-4};
+
+          //---- button1 ----
+          button1.setText(bundle.getString("AssesmentViewContent.button1.text"));
+          assessmentPanel.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
+
+          //---- button2 ----
+          button2.setText(bundle.getString("AssesmentViewContent.button2.text"));
+          assessmentPanel.add(button2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 0), 0, 0));
+
+          //---- button3 ----
+          button3.setText(bundle.getString("AssesmentViewContent.button3.text"));
+          assessmentPanel.add(button3, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 5), 0, 0));
+
+          //---- button4 ----
+          button4.setText(bundle.getString("AssesmentViewContent.button4.text"));
+          assessmentPanel.add(button4, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
+        }
+        AssessmentPanel.add(assessmentPanel, "cell 0 7 2 1");
+
+        //======== panel5 ========
+        {
+          panel5.setBorder(new TitledBorder(new LineBorder(Color.darkGray, 1, true), bundle.getString("AssesmentViewContent.panel5.border")));
+          panel5.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+          //---- label8 ----
+          label8.setText(bundle.getString("AssesmentViewContent.label8.text"));
+          panel5.add(label8);
+          panel5.add(StatisticsContainer);
+        }
+        AssessmentPanel.add(panel5, "cell 0 8 2 1");
+
+        //======== panel3 ========
+        {
+          panel3.setBorder(new TitledBorder(new LineBorder(Color.darkGray), bundle.getString("AssesmentViewContent.panel3.border")));
+          panel3.setLayout(new GridBagLayout());
+          ((GridBagLayout)panel3.getLayout()).columnWidths = new int[] {85, 0, 0};
+          ((GridBagLayout)panel3.getLayout()).rowHeights = new int[] {0, 0, 0};
+          ((GridBagLayout)panel3.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
+          ((GridBagLayout)panel3.getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0E-4};
+
+          //---- label7 ----
+          label7.setText(bundle.getString("AssesmentViewContent.label7.text"));
+          panel3.add(label7, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.EAST, GridBagConstraints.VERTICAL,
+            new Insets(0, 0, 5, 5), 0, 0));
+          panel3.add(comboBox4, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 0), 0, 0));
+
+          //======== panel4 ========
+          {
+            panel4.setLayout(new FlowLayout());
+
+            //---- button6 ----
+            button6.setText(bundle.getString("AssesmentViewContent.button6.text"));
+            panel4.add(button6);
+
+            //---- button7 ----
+            button7.setText(bundle.getString("AssesmentViewContent.button7.text"));
+            panel4.add(button7);
+          }
+          panel3.add(panel4, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
+        }
+        AssessmentPanel.add(panel3, "cell 0 9 2 1");
       }
       tabbedPane1.addTab("Assessment", AssessmentPanel);
 
@@ -164,6 +296,24 @@ public class AssesmentViewContent extends JPanel {
   private JLabel label6;
   private TextFieldWithBrowseButton autograderConfigPathInput;
   private JSeparator separator1;
+  private JPanel generalPanel;
+  private JButton btnGradingRound1;
+  private JButton btnGradingRound2;
+  private JButton button5;
+  private JPanel assessmentPanel;
+  private JButton button1;
+  private JButton button2;
+  private JButton button3;
+  private JButton button4;
+  private JPanel panel5;
+  private JLabel label8;
+  private JLabel StatisticsContainer;
+  private JPanel panel3;
+  private JLabel label7;
+  private ComboBox comboBox4;
+  private JPanel panel4;
+  private JButton button6;
+  private JButton button7;
   private JPanel GradingPanel;
   private JPanel panel1;
   private JLabel label4;
