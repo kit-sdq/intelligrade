dc64d20d9431d8a9f24a59635b1e96aaaead7386
Florian (Feuermagier) <floseligmann@web.de>
2024-09-27 14:57:57 +0200

    reapply changes

--- src/test/java/edu/kit/kastel/ArchitectureTest.java
+++ src/test/java/edu/kit/kastel/ArchitectureTest.java
@@ -11,14 +11,10 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-@AnalyzeClasses(packages = "edu.kit.kastel")
+@AnalyzeClasses(packages = "edu.kit.kastel.sdq.intelligrade")
 class ArchitectureTest {
-    private static final String ARTEMIS_4J_PACKAGE = "edu.kit.kastel.sdq.artemis4j..";
-
     @ArchTest
     static final ArchRule noForEachInCollectionsOrStream = noClasses()
-            .that()
-            .resideOutsideOfPackage(ARTEMIS_4J_PACKAGE)
             .should()
             .callMethod(Stream.class, "forEach", Consumer.class)
             .orShould()
